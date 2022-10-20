fcac4826c5b82ac216fd33e7cbe8cb70199fbddd
kovaldos <ya.kovalev@ya-kovalev.ru>
2022-10-20 17:23:02 +0300

    feat: start adding footer

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,11 @@
-import { createRouter, createWebHashHistory } from "vue-router";
-import HomeView from "../views/HomeView.vue";
+import { createRouter, createWebHistory } from "vue-router";
+import VueHeader from "../components/VueHeader.vue";
 
 const routes = [
   {
     path: "/",
     name: "home",
-    component: HomeView,
+    component: VueHeader,
   },
   {
     path: "/about",
@@ -19,7 +19,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(),
   routes,
 });
 
